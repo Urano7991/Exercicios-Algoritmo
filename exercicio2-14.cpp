51beb51b5e625d4f6873d6797c349a0114e18ad6
Vic Fonteles <vicruasf@gmail.com>
2025-09-15 15:25:48 -0300

    exercicio2-14.cpp

---
+++ exercicio2-14.cpp
@@ -0,0 +1,19 @@
+//Faça um algoritmo em C++ que solicite um número e imprima se ele está entre 21 e 30 ou não.
+#include <iostream>
+
+int main() {
+    int num;
+    
+    printf("Digite três numeros numero: ");
+    scanf("%d", &num);
+    
+    if((num > 21) && (num < 30)) 
+        {
+            printf("%d está entre 21 e 30.", num);
+        }
+    else 
+        {
+            printf("%d não está entre 21 e 30", num);
+        }
+    return 0;
+}
